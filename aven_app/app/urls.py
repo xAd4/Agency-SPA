852d6259a5570737571ad137a2291b8da7c3a477
angelestarita <101217260+xAd4@users.noreply.github.com>
2024-08-12 15:32:09 -0400

    Implemented the feature to create gallery instances for staff

--- aven_app/app/urls.py
+++ aven_app/app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
-    path("property/gallery/", views.PropertyGalleryView.as_view(), name="property_gallery"),
+    path("property/gallery/<int:pk>", views.PropertyGalleryView.as_view(), name="property_gallery"),
+    path("property/gallery/create/", views.PropertyGalleryCreateView.as_view(), name="property_gallery_create")
 ]
--- aven_app/app/views.py
+++ aven_app/app/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, FormView
+from django.utils.decorators import method_decorator
+from django.contrib.admin.views.decorators import staff_member_required
+from django.views.generic import TemplateView, FormView, DetailView, CreateView
 from .forms import AppointmentForm, ContactForm
+from .models import Gallery
+from django import forms
 
 # Create your views here.
+#* IMPORTANT: VIEW HOME
 class HomeView(FormView):
     template_name = "app/index.html"
     form_class = AppointmentForm  # Usamos AppointmentForm como el formulario principal
@@ -29,5 +34,26 @@ class HomeView(FormView):
         else:
             return self.form_invalid(appointment_form)
     
-class PropertyGalleryView(TemplateView):
+#* IMPORTANT: VIEW DETAIL GALLERY
+class PropertyGalleryView(DetailView):
+    model = Gallery
     template_name = "app/idea-details.html"
+    
+#TODO: VIEW DETAIL CREATE: Exclusive use for staff
+@method_decorator(staff_member_required, name="dispatch")
+class PropertyGalleryCreateView(CreateView):
+    model = Gallery
+    template_name = "helpers/form_create_update.html"
+    fields = ["squareFeet","garage","bedrooms","pool","build","image","title","content"]
+    success_url = reverse_lazy("status-200")
+    
+    def get_form(self, form_class=None):
+        form = super().get_form()
+        form.fields["squareFeet"].widget = forms.TextInput(attrs={"class": "form-control", 'placeholder':"Square Feet"})
+        form.fields["garage"].widget = forms.NumberInput(attrs={"class": "form-control", 'placeholder':"Garage"})
+        form.fields["bedrooms"].widget = forms.NumberInput(attrs={"class": "form-control", 'placeholder':"Bedrooms"})
+        form.fields["build"].widget = forms.TextInput(attrs={"class": "form-control", 'placeholder':"Build Year"})
+        form.fields["image"].widget = forms.ClearableFileInput(attrs={"class": "form-control-file", 'placeholder':"Image"})
+        form.fields["title"].widget = forms.TextInput(attrs={"class": "form-control", 'placeholder':"Title"})
+        form.fields["content"].widget = forms.Textarea(attrs={"class": "form-control", 'placeholder':"Content"})
+        return form
